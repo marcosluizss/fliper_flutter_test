1f3fa9b75b9f0ed2c4d932bfe13acb547de9d676
Marcos Luiz <marcosluizss@gmail.com>
2021-05-06 23:25:19 -0300

    implementa injeção de dependencia

---
+++ lib/injection_container.dart
@@ -0,0 +1,34 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:fliper_flutter_test/core/infra/network_info.dart';
+import 'package:fliper_flutter_test/features/wealth_summary/data/datasources/wealth_summary_remote_datasource.dart';
+import 'package:fliper_flutter_test/features/wealth_summary/data/repositories/wealth_summary_repository_impl.dart';
+import 'package:fliper_flutter_test/features/wealth_summary/domain/repositories/wealth_summary_repository.dart';
+import 'package:fliper_flutter_test/features/wealth_summary/domain/usecases/get_wealth_summary.dart';
+import 'package:fliper_flutter_test/features/wealth_summary/presentation/bloc/wealth_summary_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+void init() {
+  sl.registerFactory(() => WealthSummaryBloc(getWealthSummary: sl()));
+
+  sl.registerLazySingleton(() => GetWealthSummary(sl()));
+
+  sl.registerLazySingleton<WealthSummaryRepository>(
+    () => WealthSummaryRepositoryImpl(
+      networkInfo: sl(),
+      wealthSummaryRemoteDataSource: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<WealthSummaryRemoteDataSource>(
+    () => WealthSummaryRemoteDataSourceImpl(
+      client: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkIndoImpl(sl()));
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => Connectivity());
+}
