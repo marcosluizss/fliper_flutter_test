862f66d78ce2c8e308959d0f30c746c0c82fab63
Marcos Luiz <marcosluizss@gmail.com>
2021-05-06 21:31:13 -0300

    - implementa datasource

--- lib/features/wealth_summary/data/datasources/wealth_summary_remote_datasource.dart
+++ lib/features/wealth_summary/data/datasources/wealth_summary_remote_datasource.dart
@@ -1,5 +1,50 @@
+import 'dart:convert';
+
+import 'package:fliper_flutter_test/core/error/exceptions.dart';
 import 'package:fliper_flutter_test/features/wealth_summary/data/models/wealth_summary_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class WealthSummaryRemoteDataSource {
   Future<WealthSummaryModel> getWealthSummary();
+}
+
+class WealthSummaryRemoteDataSourceImpl
+    implements WealthSummaryRemoteDataSource {
+  final http.Client client;
+
+  WealthSummaryRemoteDataSourceImpl({required this.client});
+
+  final url = Uri.parse("https://harura-fliper-test.herokuapp.com/v1/graphql");
+
+  final headers = {
+    "content-type": "application/json",
+    "x-hasura-admin-secret": "fliperdevtest2020"
+  };
+
+  @override
+  Future<WealthSummaryModel> getWealthSummary() async {
+    final query = '''query MyQuery {
+                              wealthSummary {
+                                cdi
+                                gain
+                                hasHistory
+                                id
+                                profitability
+                                total
+                              }
+                            }''';
+
+    final response = await client.post(
+      url,
+      headers: headers,
+      body: {"query": query},
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return WealthSummaryModel.fromJson(data["data"]["wealthSummary"][0]);
+    } else {
+      throw ServerException();
+    }
+  }
 }
---
+++ test/features/wealth_summary/data/datasources/wealth_summary_remote_datasource_test.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+import 'package:fliper_flutter_test/core/error/exceptions.dart';
+import 'package:fliper_flutter_test/features/wealth_summary/data/datasources/wealth_summary_remote_datasource.dart';
+import 'package:fliper_flutter_test/features/wealth_summary/data/models/wealth_summary_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../fixture/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements http.Client {
+  @override
+  Future<Response> post(
+    Uri? url, {
+    Map<String, String>? headers,
+    Object? body,
+    Encoding? encoding,
+  }) =>
+      super.noSuchMethod(
+          Invocation.method(#post, [url, headers, body, encoding])) ??
+      Future.value(Response('', 500));
+}
+
+void main() {
+  late WealthSummaryRemoteDataSourceImpl remoteDataSource;
+  late MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    remoteDataSource =
+        WealthSummaryRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  final tWealthSummaryModel = WealthSummaryModel(
+    id: 2,
+    cdi: 3.45679,
+    gain: 1833.23,
+    hasHistory: true,
+    profitability: 2.76789,
+    total: 3200876,
+  );
+
+  test('''should perform a POST request on a URL and validate
+          header with application/json and secret key''', () async {
+    when(mockHttpClient.post(any, headers: any, body: any, encoding: any))
+        .thenAnswer(
+      (_) async => http.Response(
+          fixture('wealth_summary_datasource_response.json'), 200),
+    );
+
+    remoteDataSource.getWealthSummary();
+
+    verify(mockHttpClient.post(
+      Uri.parse("https://harura-fliper-test.herokuapp.com/v1/graphql"),
+      headers: {
+        "content-type": "application/json",
+        "x-hasura-admin-secret": "fliperdevtest2020"
+      },
+      body: any,
+    ));
+  });
+
+  test(
+      'should return a WealthSummaryModel when the response code is 200 ( success )',
+      () async {
+    when(mockHttpClient.post(any, headers: any, body: any, encoding: any))
+        .thenAnswer(
+      (_) async => http.Response(
+          fixture('wealth_summary_datasource_response.json'), 200),
+    );
+
+    final result = await remoteDataSource.getWealthSummary();
+
+    expect(result, tWealthSummaryModel);
+  });
+
+  test('should return a ServerException when the response code is 404 or orher',
+      () async {
+    when(mockHttpClient.post(any, headers: any, body: any, encoding: any))
+        .thenAnswer((_) async => http.Response('Somehting went wrong', 404));
+
+    final call = remoteDataSource.getWealthSummary;
+
+    expect(() => call(), throwsA(isInstanceOf<ServerException>()));
+  });
+}
